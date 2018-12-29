777a08be5e423d7eb0b8c5ee110a815d5c81429c
Minun Dragonation <minun@mewmew.cn>
2018-12-29 23:34:52 +0800

    t

--- test.js
+++ test.js
@@ -2,13 +2,13 @@ let Reader = require("./reader.js");
 
 let parse = require("./parse.js");
 
-var filePath = @.fs.resolvePath(__dirname, "romfs/bin/archive/pokemon/pm0004_00.gfpak");
+// var filePath = @.fs.resolvePath(__dirname, "romfs/bin/archive/pokemon/pm0004_00.gfpak");
 
-// var filePath = @.fs.resolvePath(__dirname, "pm0006_00.gfpak");
+var filePath = @.fs.resolvePath(__dirname, "pm0006_00.gfpak");
 
 var package = parse(new Reader(@.fs.readFile.sync(filePath)), @.fs.extname(filePath));
 
 @warn("Package parsed");
 
-// @dump(package.files.filter((x) => x.type === ".bin-18-some-like-file-list"));
+@.fs.writeFile.sync("./anim.json", JSON.stringify(package.files.filter((x) => x.type === ".bin-18-like-animation")[0].content));
 
